c7cb802535c84308969328c680d8784cb0ddda21
Ayush Kumar Singh <shkmrsingh@gmail.com>
2025-03-21 22:01:40 +0530

    created files structure and setup charkra ui redux-toolkit

--- Frontend/bewakoof/src/main.jsx
+++ Frontend/bewakoof/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider as UiProvider } from './components/ui/provider'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <UiProvider>
     <App />
-  </StrictMode>,
+  </UiProvider>,
 )
