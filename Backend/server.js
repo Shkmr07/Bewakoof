c44ce1f35f76f55bccee276644df5990de2091ef
Ayush Kumar Singh <shkmrsingh@gmail.com>
2025-03-27 20:22:19 +0530

    some changes

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json());
